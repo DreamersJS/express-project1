daee48974a61324cce2177842aeff80bf97a2254
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-08-08 21:42:10 +0300

    change: title

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173, 
     proxy: {
       // Proxy API requests to the backend server
       '/api': {
