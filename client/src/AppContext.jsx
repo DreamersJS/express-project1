7f3f93ad238b3b7fe8eb56f472a11dca0ff289ab
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2025-01-08 17:26:02 +0200

    remove redundant code

--- client/src/AppContext.jsx
+++ client/src/AppContext.jsx
@@ -44,7 +44,6 @@ export const AppContextProvider = ({ children }) => {
         setUser(userData);
         setToken(token);
         localStorage.setItem('token', token);
-        localStorage.setItem('user', JSON.stringify(userData));
     };
 
     // Function to logout
@@ -52,7 +51,6 @@ export const AppContextProvider = ({ children }) => {
         setUser(null);
         setToken(null);
         localStorage.removeItem('token');
-        localStorage.removeItem('user');
     };
 
     return (
